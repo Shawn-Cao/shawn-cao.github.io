07086942ba7ffa63bd6d7a54ec2bc57ad7ab9b48
Shawn-Cao <xiangke.cao@gmail.com>
2015-09-26 09:17:54 -0400

    Update presentation material

--- presentation/js/promiseDemo.js
+++ presentation/js/promiseDemo.js
@@ -8,15 +8,17 @@ angular.module('promiseApp', []).controller('PromiseCtrl', function(promiseFacto
         //correct way:
         promiseFactory.getPromise(data).then(function() {
             _this.promiseModel = data;
+        }, function() {
+            _this.promistModel = data;
         });
-        this.promiseModel = "waiting promise to resolve";
-    }
+        this.promiseModel = "waiting promise to resolve";  //optional waiting message
+    };
 }).factory('promiseFactory', function($q, $timeout) {
     var getPromise = function (text) {  //private
         var deferred = $q.defer();
         $timeout(function() {
-            deferred.resolve(text);
-        }, 1000);
+            deferred.resolve(text);  //.reject('intentional failure');
+        }, 1500);
         return deferred.promise;
     };
     return {
