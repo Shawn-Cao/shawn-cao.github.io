7dde71007d5581f6b55b3474ee7d890fc5787a9e
Xiangke (Shawn) Cao <xiangke.cao@gmail.com>
2015-11-21 15:59:48 -0500

    Update fun.js

--- app/angular-fun/fun.js
+++ app/angular-fun/fun.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'myApp.service.event'])
+angular.module('angualr-fun', ['ngRoute', 'myApp.service.event'])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {
